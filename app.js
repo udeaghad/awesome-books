a00472288dc0ca0b921e4293a7803f8d5f32fdda
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-24 16:59:03 +0300

    Add event lisner to remove button

---
+++ app.js
@@ -0,0 +1,43 @@
+const add = document.getElementById('add-button');
+const list = document.getElementById('list');
+const awesomBook = [];
+
+add.addEventListener('click', ()=>{
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+   
+    const liTitle = document.createElement('li');
+    const liAuthor = document.createElement('li');
+    const removeBotton = document.createElement('button');
+    const newDiv = document.createElement('div');
+
+    liTitle.classList=title.value;
+    removeBotton.classList='remove';
+    liTitle.appendChild(document.createTextNode(title.value));
+    liAuthor.appendChild(document.createTextNode(author.value));
+    removeBotton.innerText='Remove';
+    newDiv.appendChild(liTitle);
+    newDiv.appendChild(liAuthor);
+    newDiv.appendChild(removeBotton);
+    list.appendChild(newDiv);
+    const book = {
+        Title: title.value, 
+        Author: author.value
+    }
+    awesomBook.push(book); 
+    title.value=''
+    author.value = ''
+});
+
+
+list.addEventListener('click', e =>{
+    e.preventDefault();
+   
+    if(e.target.classList.contains('remove')){
+        e.target.parentElement.remove();
+        const newAwesemBook = awesomBook.filter(function(ele){
+            return ele.Title !== e.target.parentElement.children[0].innerText && (ele.Author !== e.target.parentElement.children[0].innerText);
+        })
+        console.log(newAwesemBook);
+    }
+})

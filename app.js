86763d993bdfb335d8d04f0bff4d575363fbe224
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-25 15:16:18 +0300

    Fix linter

--- app.js
+++ app.js
@@ -2,39 +2,36 @@ const add = document.getElementById('add-button');
 const list = document.getElementById('list');
 const awesomBook = [];
 
-class Info{
-  constructor(title, author){
+class Info {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
 
-function createBook(){
-  
-
+function createBook() {
   const liTitle = document.createElement('li');
   const liAuthor = document.createElement('li');
   const removeBotton = document.createElement('button');
-  const by = document.createElement('p')
+  const by = document.createElement('p');
   const newDiv = document.createElement('div');
- 
+
   newDiv.classList.add('onebook');
-  for(let i = 0;i<awesomBook.length; i += 1){  
-    newDiv.classList.toggle('removebg')
+  for (let i = 0; i < awesomBook.length; i += 1) {
+    newDiv.classList.toggle('removebg');
   }
-  
-  by.innerHTML = 'by'
+
+  by.innerHTML = 'by';
   removeBotton.classList = 'remove';
-  liTitle.className = 'title-p'
+  liTitle.className = 'title-pe';
   liTitle.appendChild(document.createTextNode(this.title.value));
   liAuthor.appendChild(document.createTextNode(this.author.value));
   removeBotton.innerText = 'Remove';
-  newDiv.append(liTitle,by,liAuthor,removeBotton);
+  newDiv.append(liTitle, by, liAuthor, removeBotton);
   list.appendChild(newDiv);
 }
-  add.addEventListener('click', () => {
-  
-const book = new Info(title.value, author.value)
+add.addEventListener('click', () => {
+  const book = new Info(title.value, author.value);
   awesomBook.push(book);
   createBook(book);
   localStorage.setItem('awesomBook', JSON.stringify(awesomBook));
@@ -43,20 +40,17 @@ const book = new Info(title.value, author.value)
   author.value = '';
 });
 
-
 list.addEventListener('click', (e) => {
   e.preventDefault();
 
   if (e.target.classList.contains('remove')) {
     e.target.parentElement.remove();
-   const newAwesemBook = awesomBook.filter(
-   (ele) => ele.Title !== e.target.parentElement.children[0].innerText,
+    const newAwesemBook = awesomBook.filter(
+      (ele) => ele.Title !== e.target.parentElement.children[0].innerText
     );
-    
+
     awesomBook.splice(awesomBook.indexOf(newAwesemBook), 1);
     localStorage.setItem('awesomBook', JSON.stringify(awesomBook));
     console.log(awesomBook);
-   
   }
 });
-

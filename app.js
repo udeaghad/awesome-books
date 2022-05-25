3575c98f99f1940a52ce63a9dcb4255fb0e67406
udeaghad <dozie.udeagha@gmail.com>
2022-05-25 11:35:05 +0100

    Add toggle style

--- app.js
+++ app.js
@@ -10,23 +10,17 @@ class Info{
 }
 
 function createBook(){
-  //const title = document.getElementById('title');
-  //const author = document.getElementById('author');
+  
 
   const liTitle = document.createElement('li');
   const liAuthor = document.createElement('li');
   const removeBotton = document.createElement('button');
   const by = document.createElement('p')
   const newDiv = document.createElement('div');
-  const count = [1,2,3,4,5,6,7,8];
-
-  //liTitle.classList = title.value;
+ 
   newDiv.classList.add('onebook');
-  for(let i = 0;i<count.length; i += 1){
-    if(count[i]%2==0){newDiv.classList.add('removebg')}
-    else{
-      newDiv.classList.add('removebg2')
-    }
+  for(let i = 1;i<awesomBook.length; i += 1){  
+    newDiv.classList.toggle('removebg')
   }
   
   by.innerHTML = 'by'
@@ -38,15 +32,12 @@ function createBook(){
   list.appendChild(newDiv);
 }
   add.addEventListener('click', () => {
-  /*const book = {
-    Title: title.value,
-    Author: author.value,
-  };*/
+  
 const book = new Info(title.value, author.value)
   awesomBook.push(book);
   createBook(book);
   localStorage.setItem('awesomBook', JSON.stringify(awesomBook));
-  //console.log(awesomBook)
+
   title.value = '';
   author.value = '';
 });
@@ -67,3 +58,4 @@ list.addEventListener('click', (e) => {
    
   }
 });
+
